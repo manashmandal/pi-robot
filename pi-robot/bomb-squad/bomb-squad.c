9f0b6dc7ecc2329ca10796e99e4c14cd5087a71f
matt.odille <matt.odille@5ecf202c-ccff-bad3-6e32-cecd19ab75c3>
2010-09-08 19:27:28 +0000

    started maze-solver; added commonfunc.h library mapping to each project

---
+++ pi-robot/bomb-squad/bomb-squad.c
@@ -0,0 +1,7 @@
+#include <pololu/3pi.h>
+#include <common-libs/commonfunc.h>
+
+int main()
+{
+	while(1);
+}
--- pi-robot/bomb-squad/default/dep/bomb-squad.o.d
+++ pi-robot/bomb-squad/default/dep/bomb-squad.o.d
@@ -1,1 +1,124 @@
-bomb-squad.o: ../bomb-squad.c
+bomb-squad.o: ../bomb-squad.c \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/3pi.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/analog.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/iom128.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanResources/include/OrangutanModel.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/buzzer.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanBuzzer/OrangutanBuzzer.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanBuzzer/../OrangutanResources/include/OrangutanModel.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/time.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/motors.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/lcd.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:\share\robotz\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/leds.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/digital.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/pushbuttons.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanPushbuttons/OrangutanPushbuttons.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanPushbuttons/../OrangutanResources/include/OrangutanModel.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/serial.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanSerial/OrangutanSerial.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanSerial/../OrangutanResources/include/OrangutanModel.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/encoders.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/resources.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanResources/OrangutanResources.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanResources/../OrangutanResources/include/OrangutanModel.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/digital.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/servos.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/pulsein.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanPulseIn/OrangutanPulseIn.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanPulseIn/../OrangutanTime/OrangutanTime.h \
+  c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanSPIMaster/OrangutanSPIMaster.h \
+  C:\share\robotz\pi-robot\pi-robot\bomb-squad\../common-libs/commonfunc.h
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/3pi.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/analog.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/iom128.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanResources/include/OrangutanModel.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/buzzer.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanBuzzer/OrangutanBuzzer.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanBuzzer/../OrangutanResources/include/OrangutanModel.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/time.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/motors.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/lcd.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+c:\share\robotz\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/leds.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/digital.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/pushbuttons.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanPushbuttons/OrangutanPushbuttons.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanPushbuttons/../OrangutanResources/include/OrangutanModel.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/serial.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanSerial/OrangutanSerial.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanSerial/../OrangutanResources/include/OrangutanModel.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/encoders.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/resources.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanResources/OrangutanResources.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanResources/../OrangutanResources/include/OrangutanModel.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/digital.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/servos.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/pulsein.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanPulseIn/OrangutanPulseIn.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanPulseIn/../OrangutanTime/OrangutanTime.h:
+
+c:/share/robotz/winavr-20100110/lib/gcc/../../avr/include/pololu/OrangutanSPIMaster/OrangutanSPIMaster.h:
+
+C:\share\robotz\pi-robot\pi-robot\bomb-squad\../common-libs/commonfunc.h:
