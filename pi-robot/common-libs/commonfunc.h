4970c6037549fb3170080b0467cba0eeba8de6c5
hazra.kaushik <hazra.kaushik@5ecf202c-ccff-bad3-6e32-cecd19ab75c3>
2010-09-15 01:15:38 +0000

    progress as of 09/14/2010

--- pi-robot/common-libs/commonfunc.h
+++ pi-robot/common-libs/commonfunc.h
@@ -8,6 +8,10 @@
 #define SENSOR_6 6
 #define SENSOR_7 7
 
+#define SENSOR_ONE 5
+#define SENSOR_TWO 6
+#define SENSOR_FRONT 7
+
 //Initializes the digital sensors
 //MODE_8_BIT => 0=far away, 150=close
 void init_sensors()
@@ -17,7 +21,7 @@ void init_sensors()
 	PORTC &= ~(1<< PORTC5);
 }
 
-//Displayes a message and waits for B button to press
+//Displays a message and waits for B button to press
 void wait_with_message(char *str)
 {
 	play("L16 ceg>c");
@@ -29,24 +33,24 @@ void wait_with_message(char *str)
 	delay_ms(500);
 }
 
-int get_value_from_user(char *msg)
+int get_value_from_user(char *msg, int default_value, int increment)
 {
-	int value = 0;
-	
+	int value = default_value;
+
 	while(1)
 	{
 		clear();
-		
+
 		// print name
 		lcd_goto_xy(0,0);
 		print(msg);
 		print("=?");
-		
+
 		// print current value
 		lcd_goto_xy(0,1);
 		print_long(value);
 		lcd_show_cursor(CURSOR_BLINKING);
-	
+
 		// wait for all buttons to be released, then a press
 		while(button_is_pressed(ANY_BUTTON));
 		char button = wait_for_button_press(ANY_BUTTON);
@@ -54,32 +58,72 @@ int get_value_from_user(char *msg)
 		if(button & BUTTON_A)
 		{
 			play("!c32");
-			value = value - 10;
+			value = value - increment;
 		}
 		else if(button & BUTTON_B)
 		{
 			lcd_hide_cursor();
 			clear();
 			play("!e32");
-
+			delay_ms(100);
 			return value;
 		}
 		else if(button & BUTTON_C)
 		{
 			play("!g32");
-			value = value + 10;
+			value = value + increment;
 		}
 
-		if(value < 0)
+		if(value < -32000)
 		{
-			value = 0;
+			value = -32000 + (32000 % increment);
+		}
+		if(value > 32000)
+		{
+			value = 32000 - (32000 % increment);
 		}
 	}
 }
 
+int get_target_difference()
+{
+
+	int s1 = 0;
+	int s2 = 0;
+
+	while(!button_is_pressed(BUTTON_B))
+	{
+		if(get_ms() % 500 == 0)
+		{
+			clear();
+			print_xy(0,0,"AlignBot");
+			lcd_goto_xy(0,1);
+			print_long(read_sensor(SENSOR_ONE)-read_sensor(SENSOR_TWO));
+		}
+	}
+
+	s1 = read_sensor(SENSOR_ONE); // 0 (far away) - 150 (close)
+	s2 = read_sensor(SENSOR_TWO); // 0 (far away) - 150 (close)
+
+	play(">g32>>c32");
+
+	return s1-s2;
+
+
+}
+
+
 //Stop both motors
 void halt()
 {
+	set_motors(0,0);
+}
+
+//Dash forward
+void dash(int power, int delay)
+{
+	set_motors(power,power);
+	delay_ms(delay);
 	set_motors(0,0);
 }
 
@@ -93,7 +137,7 @@ void turn_left(int power, int angle)
 	set_motors(0,0);
 }
 
-//Turn the robot to left
+//Turn the robot to right
 void turn_right(int power, int angle)
 {
 	int delay = (DELAY_PER_ANGLE_PER_POWER * angle)/power;
@@ -132,7 +176,7 @@ int* scan_360(int sensor)
 {
 	int i, *pdata, data[25];
 	int power = 40;
-	
+
 	for(i=0; i<25; i++)
 	{
 		// rotate LEFT 15 degrees then stop
@@ -140,11 +184,11 @@ int* scan_360(int sensor)
 		set_motors(-power,power);
 		delay_ms(delay);
 		set_motors(0,0);
-		
+
 		// scan forward and store value
 		data[i] = analog_read(SENSOR_6);
 	}
-	
+
 	pdata = &data[0];
 	return pdata;
 }
@@ -162,6 +206,11 @@ void count_down(int count)
 		clear();
 		i++;
 	}
+}
+
+int read_sensor(int sensor)
+{
+	return analog_read(sensor);
 }
 
 //Read forward sensor data
